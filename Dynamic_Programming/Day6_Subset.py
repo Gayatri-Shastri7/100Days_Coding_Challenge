9dcf9fecb329bde50fd7ee66b57ce811653939e4
Gayatri-Shastri7 <66173499+Gayatri-Shastri7@users.noreply.github.com>
2021-05-29 16:25:02 +0530

    Initial commit

---
+++ Dynamic_Programming/Day6_Subset.py
@@ -0,0 +1,65 @@
+'''
+Subset Sum Problem | DP-25
+Difficulty Level : Medium
+ 
+Given a set of non-negative integers, and a value sum, determine if there is a subset of the given set with sum equal to given sum. 
+
+Example: 
+
+Input: set[] = {3, 34, 4, 12, 5, 2}, sum = 9
+Output: True  
+There is a subset (4, 5) with sum 9.
+
+Input: set[] = {3, 34, 4, 12, 5, 2}, sum = 30
+Output: False
+There is no subset that add up to 30.
+
+'''
+# A Dynamic Programming solution for subset
+# sum problem Returns true if there is a subset of
+# set[] with sun equal to given sum
+ 
+# Returns true if there is a subset of set[]
+# with sun equal to given sum
+def isSubsetSum(set, n, sum):
+     
+    # The value of subset[i][j] will be
+    # true if there is a
+    # subset of set[0..j-1] with sum equal to i
+    subset =([[False for i in range(sum + 1)]
+            for i in range(n + 1)])
+     
+    # If sum is 0, then answer is true
+    for i in range(n + 1):
+        subset[i][0] = True
+         
+    # If sum is not 0 and set is empty,
+    # then answer is false
+    for i in range(1, sum + 1):
+         subset[0][i]= False
+             
+    # Fill the subset table in botton up manner
+    for i in range(1, n + 1):
+        for j in range(1, sum + 1):
+            if j<set[i-1]:
+                subset[i][j] = subset[i-1][j]
+            if j>= set[i-1]:
+                subset[i][j] = (subset[i-1][j] or
+                                subset[i - 1][j-set[i-1]])
+     
+    # uncomment this code to print table
+    # for i in range(n + 1):
+    # for j in range(sum + 1):
+    # print (subset[i][j], end =" ")
+    # print()
+    return subset[n][sum]
+         
+# Driver code
+if __name__=='__main__':
+    set = [3, 34, 4, 12, 5, 2]
+    sum = 9
+    n = len(set)
+    if (isSubsetSum(set, n, sum) == True):
+        print("Found a subset with given sum")
+    else:
+        print("No subset with given sum")
