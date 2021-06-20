c3761fa2ce9b08bdebfc621b3e5fc8d626fe37c7
Gayatri-Shastri7 <66173499+Gayatri-Shastri7@users.noreply.github.com>
2021-06-20 16:47:59 +0530

    Create Balanced_Parenthesis.py

---
+++ Stacks/Balanced_Parenthesis.py
@@ -0,0 +1,78 @@
+# Balanced Parantheses in Expression
+# A bracket is considered to be any one of the following characters: (, ), {, }, [, or ].
+
+# Two brackets are considered to be a matched pair if the an opening bracket (i.e., (, [, or {) occurs to the left of a closing bracket (i.e., ), ], or }) of the exact same type. There are three types of matched pairs of brackets: [], {}, and ().
+
+# A matching pair of brackets is not balanced if the set of brackets it encloses are not matched. For example, {[(])} is not balanced because the contents in between { and } are not balanced. The pair of square brackets encloses a single, unbalanced opening bracket, (, and the pair of parentheses encloses a single, unbalanced closing square bracket, ].
+
+# By this logic, we say a sequence of brackets is balanced if the following conditions are met:
+
+# It contains no unmatched brackets.
+# The subset of brackets enclosed within the confines of a matched pair of brackets is also a matched pair of brackets.
+# Given a string s, determine whether s is balanced.
+# If a string is balanced, return 1. Otherwise, return 0.
+
+# Example :
+# Input :
+# A : {[()]}
+# B : {[(])}
+# Output :
+# A : 1
+# B : 0
+class Solution:
+    def isBalanced(self, s):
+        # write your method here
+        
+        stack=[]
+        for i in s:
+            if i=='(' or i=='{' or i=='[':
+                stack.append(i)
+            elif i==')' and stack and stack[-1]=='(':
+                stack.pop()
+            elif i=='}' and stack and stack[-1]=='{':
+                stack.pop()
+            elif i==']' and stack and stack[-1]=='[':
+                stack.pop()
+            elif (i==')' or i==']' or i=='}'):
+                stack.append(i)
+        return int(len(stack)==0)
+
+#       Status
+# Test Cases Passed
+# Expected Output
+# INPUT(s):
+# {[()]}
+
+# OUTPUT(s):
+# 1
+
+# INPUT(s):
+# {[(])}
+
+# OUTPUT(s):
+# 0
+
+# INPUT(s):
+# {{[[(())]]}}
+
+# OUTPUT(s):
+# 1
+
+# Your Output
+# INPUT(s):
+# {[()]}
+
+# OUTPUT(s):
+# 1
+
+# INPUT(s):
+# {[(])}
+
+# OUTPUT(s):
+# 0
+
+# INPUT(s):
+# {{[[(())]]}}
+
+# OUTPUT(s):
+# 1
